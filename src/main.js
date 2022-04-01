9fbbc3d6b22ffd7e5151f6f470d5cd6b5bf2c012
minghua <minghuan@gmail.com>
2022-04-02 04:38:11 +0630

    modified

--- src/main.js
+++ src/main.js
@@ -17,6 +17,8 @@ import router from './router'
 import '@/icons' // icon
 import '@/permission' // permission control
 
+console.log(process.env)
+
 /**
  * If you don't want to use mock-server
  * you want to use MockJs for mock api
